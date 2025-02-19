a19c38dd6f0f9058a35fb1b986f00d25751ff61e
최유진 <yjyj0774@gmail.com>
2025-02-19 15:58:20 +0900

    스타일 적용

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,11 @@ import Processor from "./AudioProcessor";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>harmony generator</h1>
+    <div className="w-60 m-auto pt-52">
+      <h1 className="text-3xl text-center font-extrabold bg-red-300">
+        harmony generator
+      </h1>
+      <Processor />
     </div>
   );
 };
