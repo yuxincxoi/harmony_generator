d0fa7d6243805ee5c6f3f1c615900aa2601cf39b
최유진 <yjyj0774@gmail.com>
2025-02-19 15:56:25 +0900

    webpack css 설정 변경

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,16 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
       },
       {
         test: /\.(ts|tsx)$/,
