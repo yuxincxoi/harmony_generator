38be36430164c37e52be65a31f3b514e2dc36f85
최유진 <yjyj0774@gmail.com>
2025-02-20 16:25:19 +0900

    상태변화에 따른 배경화면 효과 설졍

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -6,13 +6,18 @@ const MainPage: React.FC = () => {
   const backgroundImg = "url('./img/backgroundImg.jpg')";
 
   return (
-    <div
-      className={`w-screen h-screen bg-cover transition-all duration-1000`}
-      style={{
-        backgroundImage: isProcessing ? backgroundImg : "none",
-        backgroundColor: isProcessing ? "transparent" : "white",
-      }}
-    >
+    <div className="relative w-screen h-screen">
+      <div
+        className={`absolute inset-0 w-full h-full bg-cover transition-opacity duration-1000 ease-in-out ${
+          isProcessing ? "opacity-100" : "opacity-0"
+        }`}
+        style={{ backgroundImage: backgroundImg }}
+      />
+      <div
+        className={`absolute inset-0 w-full h-full bg-white transition-opacity duration-1000 ease-in-out ${
+          isProcessing ? "opacity-0" : "opacity-100"
+        }`}
+      />
       <Processor className="relative" onProcessingChange={setIsProcessing} />
     </div>
   );
