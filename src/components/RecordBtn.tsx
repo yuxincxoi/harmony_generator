18476395663e3896bf85f738e9f2bcfc0baa6e45
최유진 <yjyj0774@gmail.com>
2025-02-20 16:16:13 +0900

    record button 디자인 수정

--- src/components/RecordBtn.tsx
+++ src/components/RecordBtn.tsx
@@ -2,12 +2,26 @@ import React from "react";
 import { RecordBtnProps } from "@/interfaces/components/RecordBtn.interface";
 
 const RecordBtn: React.FC<RecordBtnProps> = ({ isProcessing, onClick }) => {
+  const recordImg = "url('./img/recordImg.png')";
+  const backgroundImg = "url('./img/backgroundImg.jpg')";
+
   return (
     <div
-      className="cursor-pointer mt-16 mx-auto w-60 text-center p-2 bg-gray-200 hover:bg-gray-300 rounded-lg"
+      className="cursor-pointer relative w-[270px] h-[270px] flex items-center justify-center"
       onClick={onClick}
     >
-      {isProcessing ? "STOP" : "PLAY"}
+      <div className="absolute bg-white w-[270px] h-[270px] rounded-full"></div>
+      <div
+        className="absolute w-[260px] h-[260px] rounded-full bg-cover"
+        style={{ backgroundImage: backgroundImg }}
+      ></div>
+      <div className="absolute w-[245px] h-[245px] bg-white rounded-full"></div>
+      <div className="absolute bg-white rounded-full">
+        <div
+          className="w-[100px] h-[100px] bg-contain"
+          style={{ backgroundImage: isProcessing ? "hi" : recordImg }}
+        ></div>
+      </div>
     </div>
   );
 };
