e3e93bb35fa2d879dfdb148c3b87ab5bca90a7a1
최유진 <yjyj0774@gmail.com>
2025-02-21 15:47:07 +0900

    세부 디자인 수정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,7 +7,7 @@ import { PlayIcon } from "./PlayIcon";
 export const Modal: React.FC<ModalProps> = ({ onClose, audioURL }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="w-[650px] h-[500px] bg-white rounded-xl relative">
+      <div className="w-[600px] h-[450px] bg-white rounded-xl relative">
         <button
           className="absolute right-4 top-2 text-gray-700 font-thin text-lg cursor-pointer hover:text-gray-400 transition-colors"
           onClick={onClose}
