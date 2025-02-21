0dd7b2bb87001e1e11fd3e8500d2ee5a8cd1b6bb
최유진 <yjyj0774@gmail.com>
2025-02-21 17:55:40 +0900

    download 완료시 아이콘 설정

--- src/components/DownloadBtn.tsx
+++ src/components/DownloadBtn.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { DownloadBtnProps } from "@/interfaces/components/DownloadBtn.interface";
 
 const DownloadBtn: React.FC<DownloadBtnProps> = ({ audioURL }) => {
   const downloadBtn = "url('./img/downloadBtn.png')";
+  const checkImg = "url('./img/checkImg.png')";
+
+  const [isDownloaded, setIsDownloaded] = useState(false);
+
+  const handleDownloadComplete = () => {
+    setIsDownloaded(true);
+  };
 
   return (
-    <a href={audioURL} download="processed_audio.m4a">
+    <a
+      href={audioURL}
+      download="processed_audio.m4a"
+      onClick={handleDownloadComplete}
+    >
       <button
         title="download"
         className="w-5 h-5 mt-4 ml-5 bg-cover cursor-pointer"
-        style={{ backgroundImage: downloadBtn }}
+        style={{ backgroundImage: isDownloaded ? checkImg : downloadBtn }}
       ></button>
     </a>
   );
