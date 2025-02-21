708abadde7cf267f82b430473dcf09b2f284fb9c
최유진 <yjyj0774@gmail.com>
2025-02-21 17:25:54 +0900

    페이지 전환 timeout 설정

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MainPage from "./pages/mainPage";
+import IndexPage from "./pages/indexPage";
 
 const App: React.FC = () => {
-  return <MainPage />;
+  const [isIndexPage, setIsIndexPage] = useState(true);
+
+  useEffect(() => {
+    if (isIndexPage) {
+      setTimeout(() => {
+        setIsIndexPage(false);
+      }, 2500);
+    }
+  }, [isIndexPage]);
+
+  return isIndexPage ? <IndexPage /> : <MainPage />;
 };
 
 export default App;
--- src/pages/indexPage.tsx
+++ src/pages/indexPage.tsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+// IndexPage.tsx
+import React, { useState, useEffect } from "react";
 
 const IndexPage: React.FC = () => {
+  const [fadeOut, setFadeOut] = useState(false);
   const introImg = "url('./img/introImg.png')";
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFadeOut(true);
+    }, 1500);
+  }, []);
+
   return (
-    <>
-      <div className="fixed inset-0 flex items-center justify-center h-screen">
-        <div
-          className="bg-contain bg-no-repeat relative w-96 h-40 flex items-center justify-center"
-          style={{ backgroundImage: introImg }}
-        ></div>
-      </div>
-    </>
+    <div
+      className={`fixed inset-0 flex items-center justify-center h-screen transition-opacity duration-1000 ease-in-out ${
+        fadeOut ? "opacity-0" : "opacity-100"
+      }`}
+    >
+      <div
+        className="bg-contain bg-no-repeat relative w-96 h-40 flex items-center justify-center"
+        style={{ backgroundImage: introImg }}
+      ></div>
+    </div>
   );
 };
 
