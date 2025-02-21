496e9544aa7315c391b2694dfb5c670a3712145d
최유진 <yjyj0774@gmail.com>
2025-02-21 16:38:41 +0900

    모달 반응형 디자인

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,7 +9,7 @@ export const Modal: React.FC<ModalProps> = ({ onClose, audioURL }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="w-[580px] h-[400px] bg-white rounded-xl relative">
+      <div className="w-[400px] lg:w-[580px] h-[400px] bg-white rounded-xl relative">
         <button
           className="absolute right-4 top-2 text-gray-700 font-thin text-lg cursor-pointer hover:text-gray-400 transition-colors"
           onClick={onClose}
